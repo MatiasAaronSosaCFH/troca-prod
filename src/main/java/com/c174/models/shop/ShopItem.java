cd918b774208f21224ccff4e5417e80b95279eba
Matias Aaron Sosa <126860303+MatiasAaronSosaCFH@users.noreply.github.com>
2024-04-25 15:48:57 +0200

    Update ShopItem.java

--- src/main/java/com/c174/models/shop/ShopItem.java
+++ src/main/java/com/c174/models/shop/ShopItem.java
@@ -1,10 +1,11 @@
 package com.c174.models.shop;
 
 import com.c174.models.ticket.TicketShop;
-import lombok.Data;
+import lombok.*;
 
 import java.util.List;
 
+@NoArgsConstructor
 @Data
 public class ShopItem {
     private TicketShop items;
