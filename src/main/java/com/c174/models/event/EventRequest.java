dbd3dcc4f4f15e4b84b1b17d5b5c5e8fa9968726
Matias <akucfh@gmail.com>
2024-05-01 01:17:47 +0200

    fix ticket-qr

--- src/main/java/com/c174/models/event/EventRequest.java
+++ src/main/java/com/c174/models/event/EventRequest.java
@@ -1,7 +1,6 @@
 package com.c174.models.event;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +12,6 @@ import java.util.Date;
 public class EventRequest {
 
     private Long id;
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Invalid event name")
     @NotEmpty
     @Size(min = 1, max = 50)
     private String name;
